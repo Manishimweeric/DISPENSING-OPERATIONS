f1f5b9a9cf60aca64cf5ccc8a947d83d798304d5
Eric manishimwe <manishimweeric54@gmail.com>
2025-01-23 10:01:40 +0200

    update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Project\janeproject\DISPENSING-OPERATIONS\venv
+set VIRTUAL_ENV=M:\School\DISPENSING OPERATIONS\dispensing_operations\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
